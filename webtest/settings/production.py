eb190f48f68f9bc0e1f27fa18dd09cd73daf5e97
ernesto <ernesto.rp98@gmail.com>
2022-01-24 12:48:24 -0400

    Changing to use production settings

--- webtest/settings/production.py
+++ webtest/settings/production.py
@@ -11,6 +11,12 @@ SECURE_PROXY_SSL_HEADER  =  ( 'HTTP_X_FORWARDED_PROTO' ,  'https' )
 # Permitir todos los encabezados de host 
 ALLOWED_HOSTS  =  [ '*' ]
 
+
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
 try:
     from .local import *
 except ImportError:
